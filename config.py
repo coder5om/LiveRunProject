6c75885944e2683529c859d9c70727abbcdc4089
omkar <coderomkar5@gmail.com>
2024-10-07 21:16:15 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,13 @@
+# Dhan Client ID and Access Token
+client_id = "
+access_token = "
+
+# Trading Parameters
+capital = 10000  # Initial capital
+profit_threshold = 0.05  # 5% profit
+loss_threshold = -0.1  # 10% loss
+ltp_threshold = 0.03  # 3% change to trigger a trade
+security_id = "13"
+quantity = 550
+wait_time = 600  # 10 minutes in seconds
+optionPercentChangeIn30Sec = 5
